cd53f95c43c419583f545e22718e128e24da2586
Diana Cadena <diana.marcela.cadena@correounivalle.edu.co>
2024-12-03 07:48:37 -0500

    unsaved changes 2

--- sicame/empleados/urls.py
+++ sicame/empleados/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegistrarEmpleadoAPIView, MarcarAsistenciaAPIView, MarcarSalidaAPIView
+from .views import RegistrarEmpleadoAPIView, MarcarAsistenciaAPIView, MarcarSalidaAPIView, ActualizarObservacionesAPIView
 
 router = DefaultRouter()
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('registro', RegistrarEmpleadoAPIView.as_view(), name='registro-empleado'),
     path('marcar-asistencia', MarcarAsistenciaAPIView.as_view(), name='marcar-asistencia'),
     path('marcar-salida', MarcarSalidaAPIView.as_view(), name='marcar-salida'),
+    path('emocion/<int:emocion_id>/actualizar_observaciones/', ActualizarObservacionesAPIView.as_view(), name='actualizar_observaciones'),
 ]
