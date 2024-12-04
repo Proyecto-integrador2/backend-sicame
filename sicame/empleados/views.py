0127a87d05d23abbba5bfcc12b218550826237f8
Velyxius <dadawdasd@hotmail.com>
2024-12-04 05:24:36 -0500

    Added GET view to check employee emotion reports

--- sicame/empleados/views.py
+++ sicame/empleados/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from django.http import JsonResponse
 from .models import Empleado, Asistencia, Emocion
+from .serializers import EmocionSerializer
 import face_recognition
 from google.cloud import vision
+
+class VerEmocionesViewSet(ModelViewSet):
+    """
+    Vista que permite ver los reportes de los empleados
+
+    - Recibe una solictud GET del cliente
+    - Retorna la información de los reportes
+
+    Args:
+        request: La solicitud HTTP que solicita el reporte de los empleados.
+    Returns:
+        Response: Respuesta con los datos de los empleados.
+    """
+    queryset = Emocion.objects.all()
+    serializer_class = EmocionSerializer
+    http_method_names = ['get']
 
 class RegistrarEmpleadoAPIView(APIView):
     """
